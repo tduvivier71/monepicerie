8b7528a40b8fdba05660628a1dd6321fecacdddd
thierry <thierry@thierryduvivier.com>
2016-12-13 20:05:52 -0500

    accueil

--- client/app/sign/sign.client.controller.js
+++ client/app/sign/sign.client.controller.js
@@ -10,11 +10,9 @@
         .module('app.sign')
         .controller('SignController', SignController);
 
-    SignController.$inject = ['$q',
-        'toasterService', 'focus', 'authenticationService', '$location', '$auth', '$route','$window', '$timeout'];
+    SignController.$inject = ['toasterService', '$location', '$auth'];
 
-    function SignController($q,
-         toasterService, focus, authenticationService, $location, $auth, $route, $window, $timeout) {
+    function SignController(toasterService, $location, $auth) {
 
         var vm = this;
 
@@ -26,35 +24,25 @@
         vm.error = '';          // string
 
         vm.authenticate = authenticate;
-        vm.signIn = signIn;
-        vm.signInFb = signInFb;
         vm.signOut = signOut;
         vm.signUp = signUp;
 
         vm.isAuthentified = $auth.isAuthenticated();
 
         // ************************************************************************************************************/
-        // Entry point function
-        // ************************************************************************************************************/
-
-        _init();
-
-        // ************************************************************************************************************/
-        // Object configuration
-        // ************************************************************************************************************/
-
-        // ************************************************************************************************************/
         // Public function
         // ************************************************************************************************************/
 
+        /**
+         * Connection
+         * @param {string} provider - facebook, google
+         */
         function authenticate(provider) {
-            $auth.authenticate(provider).then(function(response) {
-                  //  $auth.setToken(response.access_token);
-                 //   toasterService.success('Connection réussie');
+            $auth.authenticate(provider)
+                .then(function () {
                     $location.path('/accueil');
-                 //   $window.location.reload();
                 })
-                .catch(function(error) {
+                .catch(function (error) {
                     if (error.message) {
                         // Satellizer promise reject error.
                         toasterService.error(error.message);
@@ -65,138 +53,54 @@
                         toasterService.error(error);
                     }
                 });
-        };
-
-        function signIn() {
-
-            var credentials = {
-                courriel: vm.courriel,
-                motDePasse: vm.motDePasse
-            };
-
-            $auth.login(credentials)
-                .then(function() {
-                 //   toasterService.success('You have successfully signed in!');
-                    $location.path('/accueil');
-                  //  $window.location.reload();
-                })
-                .catch(function(error) {
-                    toasterService.error(error.data.message, error.status);
-                });
         }
 
         /**
          * Déconnexion
          */
         function signOut() {
-
             if (!$auth.isAuthenticated()) {
                 return;
             }
-
-            $auth.logout()
-                .then(function() {
-                 //   toasterService.info('Déconnexion réussie');
-                    $location.path('/signout');
-                    //$window.location.reload();
-                    //$location.path('/signin');
-
-                });
+            $auth.logout().then(function () {
+                $location.path('/signout');
+            });
         }
 
 
-        /*function signIn() {
+        /**
+         * Inscription
+         * @param item
+         */
+        function signUp(item) {
 
             var credentials = {
-                courriel: vm.courriel,
-                motDePasse: vm.motDePasse
-            };
-
-            authenticationService
-                .login(credentials)
-                .error(function(error){
-                    toasterService.error(error.message);
-                })
-                .then(function(result){
-                    toasterService.info(result.data.prenom + ' '  + result.data.nom + ' est connecté avec succès');
-                    $location.path("/accueil");
-                });
-
-        } */
-
-        function signInFb() {
-
-            var credentials = {
-                courriel: vm.courriel,
-                motDePasse: vm.motDePasse
-            };
-
-            authenticationService
-                .loginFb(credentials)
-                .error(function(error){
-                    toasterService.error(error);
-                })
-                .then(function(result){
-                    toasterService.info(result.data.prenom + ' '  + result.data.nom + ' est connecté avec succès');
-                    $location.path("/accueil");
-                });
-
-        }
-
-        function signUp(_item) {
-
-            var credentials = {
-                nom : _item.nom,
-                prenom : _item.prenom,
-                courriel: _item.courriel,
-                motDePasse: _item.motDePasse
+                nom : item.nom,
+                prenom : item.prenom,
+                courriel: item.courriel,
+                motDePasse: item.motDePasse
             };
 
             $auth.signup(credentials)
-                .then(function(response) {
+                .then(function (response) {
                     $auth.setToken(response);
                     $location.path('/accueil');
                     toasterService.info('You have successfully created a new account and have been signed-in');
                 })
-                .catch(function(response) {
-                    toasterService.error(response.data.message);
+                .catch(function (error) {
+                    if (error.message) {
+                        // Satellizer promise reject error.
+                        toasterService.error(error.message);
+                    } else if (error.data) {
+                        // HTTP response error from server
+                        toasterService.error(error.data.message, error.status);
+                    } else {
+                        toasterService.error(error);
+                    }
                 });
 
-        /*    authenticationService
-                .register(credentials)
-                .error(function(error){
-                    toasterService.error(error);
-                    focus('nom_focus');
-                })
-                .then(function(result){
-                    toasterService.info(result.data.prenom + ' '  + result.data.nom + ' est enregistré avec succès');
-                    $location.path("/accueil");
-                }); */
-
         }
 
-  /*      function signOut() {
-            authenticationService.logout();
-        } */
-
-        // ************************************************************************************************************/
-        // Private function
-        // ************************************************************************************************************/
-
-        function _init() {
-
-
-
-            if ($location.path() === '/signin') {
-                var token = authenticationService.getToken();
-                if (token) {
-                    $location.path("/accueil");
-                }  else {
-                    $location.path("/signin");
-                }
-            }
-
-        }
     }
 
 })();
