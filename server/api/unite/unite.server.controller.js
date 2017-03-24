501d7c57dbc8ad664da2834414c675376c213619
thierry <thierry@thierryduvivier.com>
2017-03-23 20:07:19 -0400

    accueil

--- server/api/unite/unite.server.controller.js
+++ server/api/unite/unite.server.controller.js
@@ -4,44 +4,44 @@ var mongoose = require('mongoose'),
 	helpers = require('../shared/helpers.server.controller.js'),
 	Model = mongoose.model('Unite'); // MODIFY
 
-exports.find = function(req, res) {
-      Model.find({})
-    //  Model.find({utilisateurId: req.user} )
-		.sort('unite')
-		//.populate('coutParId')
-		.exec(function(err, data) {
-			if (err) {return res.status(400).json(err);}
-			res.status(200).json(data);
-		});
+exports.find = function (req, res) {
+
+    Model.find({utilisateurId: req.user})
+        .sort('unite')
+        .exec(function (err, data) {
+
+            if (err) {
+                return res.status(400).json(err);
+            }
+
+            res.status(200).json(data);
+        });
+
 };
 
 exports.findOne = function(req, res) {
 	helpers.findOne(req, res, Model);
 };
 
-exports.createOne = function(req, res) {
+exports.createOne = function (req, res) {
 
-    var data  = new Model({
+    var data = new Model({
         unite: req.body.unite, // A MODIFIER
-    	abreviation: req.body.abreviation, // A MODIFIER
-    	operation: req.body.operation, // A MODIFIER
-    	nombre: req.body.nombre, // A MODIFIER
-    	coutParId: req.body.coutParId, // A MODIFIER
+        abreviation: req.body.abreviation, // A MODIFIER
+        operation: req.body.operation, // A MODIFIER
+        nombre: req.body.nombre, // A MODIFIER
+        coutParId: req.body.coutParId, // A MODIFIER
         utilisateurId: req.user
     });
 
-    data.save(function(err, data) {
-        if (err) {return res.status(400).json(err);}
+    data.save(function (err, data) {
+
+        if (err) {
+            return helpers.handleSaveError(res, err, req.body.unite);
+        }
 
         res.status(201).json(data);
 
-        // use findOne for re-populate
-        // Model.findOne( { _id: data.id })
-        //     .populate('coutParId')
-        //     .exec(function(err, data) {
-        //         res.status(201).json(data);
-        //         console.log('Unite createOne : ' + data);
-        //     });
     });
 };
 
@@ -51,40 +51,48 @@ exports.deleteOne = function(req, res) {
 	helpers.deleteOne(req, res, Model);
 };
 
-exports.updateOne = function(req, res) {
-	// console.log(' exports.updateOne  ' + JSON.stringify(req.body.categorie));
+exports.updateOne = function (req, res) {
 
-	Model.findOne({_id: req.params.id})
+    Model.findOne({
+        utilisateurId: req.user,
+        _id: req.params.id
+        }, function (err, data) {
 
-		.exec(function(err, data) {
             if (err) {
                 return res.status(400).json(err);
             }
             if (!data) {
                 return res.status(404).json();
             }
+
             data.unite = req.body.unite; // A MODIFIER
             data.abreviation = req.body.abreviation; // A MODIFIER
             data.operation = req.body.operation; // A MODIFIER
             data.nombre = req.body.nombre; // A MODIFIER
             data.coutParId = req.body.coutParId; // A MODIFIER
 
-				data.save(function(err, data){
-                    if (err) {console.log('Erreur ' + err);}
+            data.save(function (err, data) {
 
-                    Model.findOne( { _id: req.params.id })
-                        .populate('coutParId')
-                        .exec(function(err, data) {
-                            if (err) {console.log('Erreur ' + err);}
-                            res.status(200).json(data);
-                            console.log('Unite updateOne : ' + data);
-                        });
+                if (err) {
+                    return helpers.handleSaveError(res, err, req.body.unite);
+                }
+
+                Model.findOne({
+                    _id: req.params.id,
+                    utilisateurId: req.user,
+                })
+                    .populate('coutParId')
+                    .exec(function (err, data) {
+                        if (err) {
+                            return res.status(400).json(err);
+                        }
+                        res.status(200).json(data);
+                    });
 
 
-				});
+            });
 
-
-		});
+        });
 
 };
 
