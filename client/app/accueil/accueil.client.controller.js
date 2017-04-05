9039736ae755bc4f951291a1f51d76255b437a08
Thierry <thierry_duvivier@hotmail.com>
2017-04-05 17:22:26 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	client/app/produit/produit.template.html

--- client/app/accueil/accueil.client.controller.js
+++ client/app/accueil/accueil.client.controller.js
@@ -6,15 +6,58 @@
         .module('app.accueil')
         .controller('AccueilController', AccueilController);
 
-    AccueilController.$inject = ['$auth'];
+    AccueilController.$inject = ['$auth','$scope','$log', '$filter', '$location', '$http', '$q', '$sce',
+        'toasterService', 'produitService', 'categorieService', 'focus', 'uniteService', 'formatService', 'epicerieService','marqueService'];
 
-    function AccueilController($auth) {
+        function AccueilController($auth, $scope, $log, $filter, $location, $http, $q, $sce,
+                                   toasterService, produitService, categorieService, focus, uniteService, formatService, epicerieService, marqueService) {
+
 
         var vm = this;
 
         vm.isAuthentified = $auth.isAuthenticated();
 
-        console.log('AccueilController');
+        vm.item = {};
+        vm.form = {};           // Object
+        vm.items = [];          // List of object
+
+        vm.inputOptionsProduit = {
+            placeholder: "Sélectionnez un produit...",
+            dataTextField: "produit",
+            dataValueField: "produit",
+            filter:"contains",
+            valuePrimitive: false, // false obligatoire car c est un objet
+            autoBind: false, // obligatoire
+            dataSource: {
+                transport: {
+                    read: function (e) {
+                        $http.get('/api/produit')
+                            .then(function success(response) {
+                                e.success(response.data);
+                            }, function error(response) {
+                                alert('something went wrong')
+                                console.log(response);
+                            });
+                    }
+                }
+            },
+            clearButton: true,
+            delay: 50,
+        //    noDataTemplate: vm.no_templateCat2,
+            suggest: true,
+            highlightFirst: true,
+            change : function(e) {
+                if (this.select() < 0) {
+                    this.value("");
+                }
+                else {
+                    console.log('XX');
+                   // vm.item.categorieId.categorie = this.text();
+                }
+            }
+        };
+
+
 
     }
 
