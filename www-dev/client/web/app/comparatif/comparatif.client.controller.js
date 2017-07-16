a8a51de791917f9cfb47f2149a798474c99132a7
thierry <thierry@thierryduvivier.com>
2017-07-16 17:36:28 -0400

    @atHome

--- www-dev/client/web/app/comparatif/comparatif.client.controller.js
+++ www-dev/client/web/app/comparatif/comparatif.client.controller.js
@@ -25,10 +25,20 @@
         vm.item = {};           // Object
         vm.items = [];          // List of object
         vm.form = {};           // Object
+        vm.start = false;
+
+
+        vm.gridOptions = {
+            enableSorting: true,
+            enableFiltering: true
+        };
+
+
+        vm.loadGrid = loadGrid;
 
         vm.myData = [{
-            "firstName": "Cox",
-            "lastName": "Carney"
+            "prenom": "Cox",
+            "nom": "Carney"
         }];
 
 
@@ -42,19 +52,45 @@
 
         function _init() {
 
+            vm.produits = produitService.query();
+
             produitService.query('', function (result) {
-              //  vm.items = result;
+                vm.items = result;
+            });
+
+        }
+
+
+        vm.highlightFilteredHeader = function( row, rowRenderIndex, col, colRenderIndex ) {
+            if( col.filters[0].term ){
+                return 'header-filtered';
+            } else {
+                return '';
+            }
+        };
+
+
+        function loadGrid() {
+
+            if (vm.start) {
 
                 vm.gridOptions = {
                     enableSorting: true,
+                    enableFiltering: true,
+                    onRegisterApi: function(gridApi){
+                        vm.gridApi = gridApi;
+                    },
                     columnDefs: [
-                        { name:'produit', field: 'produit' }
+                        { name:'Produit', field: 'produit', headerCellClass: vm.highlightFilteredHeader },
+                        { name:'Marque', field: 'marqueId.marque'},
+                        { name:'Catégorie', field: 'categorieId.categorie'},
+                        { name:'Conditionnement', field: 'fullConditionnement'},
                     ],
-                    data :  result
+
+                    data : vm.items
                 };
 
-
-            });
+            }
 
         }
 
