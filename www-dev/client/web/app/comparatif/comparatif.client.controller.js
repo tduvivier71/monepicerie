91ba0ac1f4f458d08ae30482c45a4398e4fbf981
Thierry <thierry_duvivier@hotmail.com>
2017-07-18 19:04:05 -0400

    Surface

--- www-dev/client/web/app/comparatif/comparatif.client.controller.js
+++ www-dev/client/web/app/comparatif/comparatif.client.controller.js
@@ -100,17 +100,45 @@
                 var i = 0;
                 while (i < result.length) {
 
-                 //    vm.temp.produit = result[i].produit;
-                 // //   vm.temp.marque = result[i].marqueId.marque;
-                 //    vm.temp.categorie = result[i].categorieId.categorie;
-                 //    vm.temp.conditionnement = result[i].fullConditionnement;
+                    vm.temp = {};
 
-                    vm.items.push({
+                     vm.temp.produit = result[i].produit;
+                     vm.temp.marque =  typeof  result[i].marqueId === 'undefined' ?  '' : result[i].marqueId.marque
+                     vm.temp.categorie = result[i].categorieId.categorie;
+                     vm.temp.conditionnement = result[i].fullConditionnement;
+
+                     if (result[i].historiques.length > 0) {
+
+                         var j = 0;
+                         while (j < result[i].historiques.length) {
+
+                             vm.temp2 = {};
+
+                             vm.temp2.produit = vm.temp.produit;
+                             vm.temp2.marque =  vm.temp.marque;
+                             vm.temp2.categorie = vm.temp.categorie;
+                             vm.temp2.conditionnement = vm.temp.conditionnement;
+                             vm.temp2.epicerie = result[i].historiques[j].epicerieId.epicerie;
+                             vm.temp2.prix = result[i].historiques[j].prix;
+
+                             vm.items.push(vm.temp2);
+
+                             j++;
+                         }
+
+                     } else {
+                         vm.items.push(vm.temp);
+                     }
+
+
+                 /*   vm.items.push({
                         produit : result[i].produit,
                         categorie : result[i].categorieId.categorie,
                         conditionnement : result[i].fullConditionnement
                         // ,  //marque : result[i].marqueId === '' ?  '' : result[i].marqueId.marque
-                    });
+                    }); */
+
+
                     i++;
 
 
